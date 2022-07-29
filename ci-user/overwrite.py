6e75cf6e45edb655cd7f3b6ad55ff020467935ce
jackyLiu <18922251299@163.com>
2022-07-30 00:35:34 +0800

    first Version

--- ci-user/overwrite.py
+++ ci-user/overwrite.py
@@ -22,7 +22,8 @@ elif chapter <= 8:
 lines = []
 with open("../os/Cargo.toml", 'r') as f:
     for line in f.readlines():
-        processed = line.replace(' git = "https://github.com/rcore-os/riscv"', ' path = "../ci-user/riscv" ')
+        # processed = line.replace(' git = "https://github.com/rcore-os/riscv"', ' path = "../ci-user/riscv" ')
+        processed = line
         lines.append(processed)
 with open("../os/Cargo.toml", 'w+') as f:
     f.writelines(lines)
